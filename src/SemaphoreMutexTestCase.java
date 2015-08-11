816d4a8ec46c74a7e45116615416a252ed66a0b6
XIN <xsuhust@gmail.com>
2015-08-10 21:12:11 -0700

    implement Producer and Consumer classes,

--- src/SemaphoreMutexTestCase.java
+++ src/SemaphoreMutexTestCase.java
@@ -17,11 +17,74 @@ public class SemaphoreMutexTestCase {
     static Semaphore semaphore = new Semaphore(0);
     static Semaphore mutex = new Semaphore(1);
 
+    // produce Integer every time
     static class Producer extends Thread{
         public void run(){
 
             int counter = 1;
+            try{
+                while(true){
+                    String threadName = Thread.currentThread().getName() + counter++;
 
+                    mutex.acquire();
+                    list.add(threadName);
+                    System.out.println("Producer is producing new value: " + threadName);
+                    mutex.release();
+
+                    // release lock
+                    semaphore.release();
+                    Thread.sleep(500);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
         }
     }
+
+    // consume Integer every time
+    static class Consumer extends Thread{
+
+        String consumerName;
+
+        public Consumer(String name){
+            this.consumerName = name;
+        }
+
+        public void run(){
+            try{
+                while(true){
+                    /**
+                     * Acquire lock. Acquires the given number of permits from this semaphore,
+                     * blocking until all are available
+                     * process stops here until producer releases the lock
+                     */
+                    semaphore.acquire();
+
+                    // Acquires a permit from this semaphore, blocking until one is available
+                    mutex.acquire();
+                    String result = "";
+                    for(String value : list){
+                        result = value + ",";
+                    }
+                    System.out.println(consumerName + "consumes value: " + result + "list.size():" + list.size());
+                    mutex.release();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        new Producer().start();
+        new Consumer("Apple").start();
+        new Consumer("Google").start();
+        new Consumer("Facebook").start();
+    }
+
+    /**
+     *  This process is like below:
+     *  Producer: Add an object to list --> Semaphore.release(1)
+     *  Consumer someone: Semaphore.acquire(1) -->  get access to read what is inside the list
+     */
 }
